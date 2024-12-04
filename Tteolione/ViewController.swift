30c068d33189046f51dabc581e79f01d8bba7f92
JunYeong-J <eeot2827@gmail.com>
2024-12-04 22:37:04 +0900

    init: 초기설정

---
+++ Tteolione/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tteolione
+//
+//  Created by 전준영 on 12/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
