e022f5b2f2dfc90fdf8da6db7de59de3534811a1
JunYeong-J <eeot2827@gmail.com>
2024-12-05 14:38:57 +0900

    feat: BaseView, BaseViewController 설정

---
+++ Tteolione/Sources/Base/BaseView.swift
@@ -0,0 +1,28 @@
+//
+//  BaseView.swift
+//  Tteolione
+//
+//  Created by 전준영 on 12/5/24.
+//
+
+import UIKit
+
+class BaseView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureHierarchy()
+        configureLayout()
+        configureView()
+    }
+    
+    func configureHierarchy() { }
+    func configureLayout() { }
+    func configureView() { }
+    
+    @available(*,unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
