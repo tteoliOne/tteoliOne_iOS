dec2dbf4644afd8eda3c24967c38d17710c94646
JunYeong-J <eeot2827@gmail.com>
2024-12-28 22:31:01 +0900

    refactor: 회원가입 requestBody 수정

--- Tteolione/Sources/Network/Models/OnBoarding/Requests/JoinRequestBody.swift
+++ Tteolione/Sources/Network/Models/OnBoarding/Requests/JoinRequestBody.swift
@@ -11,17 +11,20 @@ struct JoinRequestBody: Encodable {
     
     let email: String?
     let code: String?
+    let username: String?
     let loginId: String?
     let nickname: String?
     let password: String?
     
     init(email: String? = nil,
          code: String? = nil,
+         username: String? = nil,
          loginId: String? = nil,
          nickname: String? = nil,
          password: String? = nil) {
         self.email = email
         self.code = code
+        self.username = username
         self.loginId = loginId
         self.nickname = nickname
         self.password = password
