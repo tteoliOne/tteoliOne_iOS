dcadfc22bb51658f8e65729ca47a22fc17f6ea15
JunYeong-J <eeot2827@gmail.com>
2025-01-03 14:17:21 +0900

    feat: 아이디 인증 뷰 UI 생성

--- Tteolione/Sources/Scenes/LoginView/FindIDView/ViewController/FindIDAuthViewController.swift
+++ Tteolione/Sources/Scenes/LoginView/FindIDView/ViewController/FindIDAuthViewController.swift
@@ -5,4 +5,30 @@
 //  Created by 전준영 on 1/2/25.
 //
 
-import Foundation
+import UIKit
+import ReactorKit
+import RxCocoa
+
+final class FindIDAuthViewController: BaseViewController<FindIDAuthView> {
+    
+    var disposeBag = DisposeBag()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: true)
+    }
+}
+
+extension FindIDAuthViewController: View {
+    
+    func bind(reactor: LoginReactor) {
+//        bindAction(reactor)
+//        bindState(reactor)
+//        bindNavigation(reactor)
+    }
+    
+}
