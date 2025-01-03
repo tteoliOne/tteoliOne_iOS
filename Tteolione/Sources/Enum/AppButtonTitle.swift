ac90a249ba0ac7e22a92d6c26bbe1fea9bdad8ff
JunYeong-J <eeot2827@gmail.com>
2025-01-03 16:25:45 +0900

    feat: appButtonTitle enum 추가

--- Tteolione/Sources/Enum/AppButtonTitle.swift
+++ Tteolione/Sources/Enum/AppButtonTitle.swift
@@ -12,6 +12,7 @@ enum AppButtonTitle: String {
     case login = "로그인"
     case signUp = "회원가입"
     case resetPassword = "비밀번호 변경"
+    case completeResetPassword = "변경하기"
     case findID = "아이디 찾기"
     case email = "인증번호 전송"
     case authNum = "인증완료"
