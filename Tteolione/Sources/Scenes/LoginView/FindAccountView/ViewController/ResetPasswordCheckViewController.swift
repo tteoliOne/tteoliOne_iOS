4d5548e0a7e94b7093c38c2ef97cc6ee93bbafe5
JunYeong-J <eeot2827@gmail.com>
2025-01-03 16:26:23 +0900

    feat: 비밀번호 변경 UI 구현

--- Tteolione/Sources/Scenes/LoginView/FindAccountView/ViewController/ResetPasswordCheckViewController.swift
+++ Tteolione/Sources/Scenes/LoginView/FindAccountView/ViewController/ResetPasswordCheckViewController.swift
@@ -5,4 +5,30 @@
 //  Created by 전준영 on 1/3/25.
 //
 
-import Foundation
+import UIKit
+import ReactorKit
+import RxCocoa
+
+final class ResetPasswordCheckViewController: BaseViewController<ResetPasswordCheckView> {
+    
+    var disposeBag = DisposeBag()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: true)
+    }
+}
+
+extension ResetPasswordCheckViewController: View {
+    
+    func bind(reactor: LoginReactor) {
+//        bindAction(reactor)
+//        bindState(reactor)
+//        bindNavigation(reactor)
+    }
+    
+}
