1e2987c15754e6162e3d5648aad400136be488e6
JunYeong-J <eeot2827@gmail.com>
2025-01-09 21:07:20 +0900

    feat: baseCell 추가

--- Tteolione/Sources/Base/BaseCollectionViewCell.swift
+++ Tteolione/Sources/Base/BaseCollectionViewCell.swift
@@ -5,4 +5,23 @@
 //  Created by 전준영 on 1/9/25.
 //
 
-import Foundation
+import UIKit
+
+class BaseCollectionViewCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureHierarchy()
+        configureLayout()
+        configureView()
+    }
+    
+    func configureHierarchy() { }
+    func configureLayout() { }
+    func configureView() { }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- Tteolione/Sources/Base/BaseTableViewCell.swift
+++ Tteolione/Sources/Base/BaseTableViewCell.swift
@@ -5,4 +5,24 @@
 //  Created by 전준영 on 1/9/25.
 //
 
-import Foundation
+import UIKit
+
+class BaseTableViewCell: UITableViewCell {
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configureHierarchy()
+        configureLayout()
+        configureView()
+    }
+    
+    func configureHierarchy(){ }
+    func configureLayout(){ }
+    func configureView(){ }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
