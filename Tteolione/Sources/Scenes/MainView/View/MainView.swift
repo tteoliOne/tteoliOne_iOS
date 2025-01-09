7b9849127014847ce37056cbefe85c6530866a88
JunYeong-J <eeot2827@gmail.com>
2025-01-09 21:09:26 +0900

    feat: 메인뷰 UI구성 및 더미로 뷰생성

--- Tteolione/Sources/Scenes/MainView/View/MainView.swift
+++ Tteolione/Sources/Scenes/MainView/View/MainView.swift
@@ -5,4 +5,82 @@
 //  Created by 전준영 on 1/9/25.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+final class MainView: BaseView {
+    
+    let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(MainTableViewCell.self,
+                           forCellReuseIdentifier: MainTableViewCell.identifier)
+        tableView.separatorStyle = .none
+        tableView.rowHeight = 320
+        return tableView
+    }()
+    let postButton = PostButton()
+    
+    override func configureHierarchy() {
+        [tableView, postButton].forEach { addSubview($0) }
+    }
+    
+    override func configureLayout() {
+        tableView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        postButton.snp.makeConstraints { make in
+            make.trailing.bottom.equalTo(safeAreaLayoutGuide).inset(10)
+            make.height.equalTo(48)
+            make.width.equalTo(120)
+        }
+    }
+    
+}
+
+extension MainView {
+    
+    func adjustButtonShape(forScrollOffset offset: CGFloat) {
+        DispatchQueue.main.async {
+            let isCompact = offset > 100
+            
+            UIView.animate(withDuration: 0.3) {
+                if isCompact {
+                    self.postButton.snp.updateConstraints { make in
+                        make.width.equalTo(48)
+                    }
+                    self.postButton.setTitle("", for: .normal)
+                } else {
+                    self.postButton.snp.updateConstraints { make in
+                        make.width.equalTo(120)
+                    }
+                    self.postButton.setTitle("상품등록", for: .normal)
+                }
+                self.layoutIfNeeded()
+            }
+        }
+    }
+    
+}
+
+#if DEBUG
+
+import SwiftUI
+
+struct ViewControllerPresentable: UIViewControllerRepresentable{
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+
+    }
+
+    func makeUIViewController(context: Context) -> some UIViewController {
+        MainViewController()
+    }
+}
+
+struct ViewControllerPrepresentable_PreviewProvider : PreviewProvider{
+    static var previews: some View{
+        ViewControllerPresentable()
+    }
+}
+
+#endif
--- Tteolione/Sources/Scenes/MainView/ViewController/MainViewController.swift
+++ Tteolione/Sources/Scenes/MainView/ViewController/MainViewController.swift
@@ -5,4 +5,74 @@
 //  Created by 전준영 on 1/9/25.
 //
 
-import Foundation
+import UIKit
+import ReactorKit
+import RxCocoa
+
+final class MainViewController: BaseViewController<MainView> {
+    
+    var disposeBag = DisposeBag()
+    
+    private let dummyCategories = [
+        "Category 1",
+        "Category 2",
+        "Category 3",
+        "Category 4",
+        "Category 5",
+        "Category 6"
+    ]
+    
+    private let dummyProducts = [
+        [
+            Product(name: "Onion", price: 4000, distance: "35m 도보 5분"),
+            Product(name: "Tomato", price: 3000, distance: "100m 도보 10분"),
+            Product(name: "Apple", price: 5000, distance: "50m 도보 8분"),
+            Product(name: "Banana", price: 2000, distance: "80m 도보 15분"),
+            Product(name: "Carrot", price: 2500, distance: "20m 도보 3분")
+        ],
+        [
+            Product(name: "Milk", price: 2500, distance: "40m 도보 7분"),
+            Product(name: "Bread", price: 1500, distance: "20m 도보 5분"),
+            Product(name: "Cheese", price: 3500, distance: "70m 도보 12분")
+        ],
+        [
+            Product(name: "Potato", price: 1500, distance: "60m 도보 12분")
+        ],
+        [
+            Product(name: "Cabbage", price: 2200, distance: "30m 도보 6분")
+        ],
+        [
+            Product(name: "Lettuce", price: 1800, distance: "25m 도보 5분")
+        ],
+        [
+            
+        ]
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let dataSource = Observable.just(
+            zip(dummyCategories, dummyProducts)
+                .map { MainSectionModel(category: $0, products: $1) }
+        )
+        
+        dataSource
+            .bind(to: rootView.tableView.rx.items(
+                cellIdentifier: MainTableViewCell.identifier,
+                cellType: MainTableViewCell.self
+            )) { index, section, cell in
+                cell.configure(category: section.category, products: section.products)
+            }
+            .disposed(by: disposeBag)
+        
+        Observable.merge(
+            rootView.tableView.rx.contentOffset.map { $0.y }
+        )
+        .bind(with: self) { owner, offset in
+            owner.rootView.adjustButtonShape(forScrollOffset: offset)
+        }
+        .disposed(by: disposeBag)
+    }
+    
+}
