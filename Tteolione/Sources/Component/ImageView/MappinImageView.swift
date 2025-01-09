2907a46956d87fbf20a314c743403365627e9c01
JunYeong-J <eeot2827@gmail.com>
2025-01-09 21:08:53 +0900

    feat: 맵핀 이미지뷰 생성

--- Tteolione/Sources/Component/ImageView/MappinImageView.swift
+++ Tteolione/Sources/Component/ImageView/MappinImageView.swift
@@ -5,4 +5,19 @@
 //  Created by 전준영 on 1/9/25.
 //
 
-import Foundation
+import UIKit
+
+class MappinImageView: UIImageView {
+    
+    init() {
+        super.init(frame: .zero)
+        self.image = UIImage(systemName: "mappin")?
+            .withTintColor(.myAppLightGray2, renderingMode: .alwaysOriginal)
+        self.contentMode = .scaleAspectFit
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
