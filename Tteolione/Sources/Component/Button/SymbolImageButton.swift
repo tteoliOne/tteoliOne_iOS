b22b7397a7487fe60c2363492e7ee9cdf7527bed
JunYeong-J <eeot2827@gmail.com>
2025-01-10 17:49:48 +0900

    feat: symbol이미지 버튼 공통으로 사용하도록 만듦

--- Tteolione/Sources/Component/Button/SymbolImageButton.swift
+++ Tteolione/Sources/Component/Button/SymbolImageButton.swift
@@ -5,4 +5,26 @@
 //  Created by 전준영 on 1/10/25.
 //
 
-import Foundation
+import UIKit
+
+class SymbolImageButton: UIButton {
+    
+    init(name: String) {
+        super.init(frame: .zero)
+        backgroundColor = .white
+        let imageConfig = UIImage.SymbolConfiguration(pointSize: 30, weight: .light)
+        setImage(UIImage(systemName: name, withConfiguration: imageConfig)?.withTintColor(.black, renderingMode: .alwaysOriginal), for: .normal)
+        imageView?.contentMode = .scaleAspectFit
+        layer.shadowColor = UIColor.black.cgColor
+        layer.masksToBounds = false
+        layer.shadowOffset = CGSize(width: 0, height: 4)
+        layer.shadowRadius = 4
+        layer.shadowOpacity = 0.3
+        layer.cornerRadius = 20
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
