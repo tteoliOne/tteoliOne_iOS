b33b77f8dcf1279e3532828228f55cdbeb38be19
JunYeong-J <eeot2827@gmail.com>
2025-01-10 17:51:08 +0900

    feat: 공통 사용할 그림자 뷰 생성

--- Tteolione/Sources/Component/View/ShadowView.swift
+++ Tteolione/Sources/Component/View/ShadowView.swift
@@ -5,4 +5,34 @@
 //  Created by 전준영 on 1/10/25.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+class ShadowView: UIView {
+
+    private let roundedView = UIView()
+
+    init() {
+        super.init(frame: .zero)
+
+        backgroundColor = .clear
+        layer.shadowColor = UIColor.myAppBlack.cgColor
+        layer.shadowOffset = CGSize(width: 0, height: 4)
+        layer.shadowRadius = 5
+        layer.shadowOpacity = 0.3
+
+        roundedView.backgroundColor = .white
+        roundedView.layer.cornerRadius = 20
+        roundedView.layer.masksToBounds = true
+
+        addSubview(roundedView)
+        roundedView.snp.makeConstraints { make in
+            make.edges.equalTo(self)
+        }
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
