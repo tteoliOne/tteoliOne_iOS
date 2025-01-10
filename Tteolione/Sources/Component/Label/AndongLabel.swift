5658b439ff666f150f60e8b0f6e7767b9f6449ae
JunYeong-J <eeot2827@gmail.com>
2025-01-10 17:50:09 +0900

    feat: 공통 label 생성

--- Tteolione/Sources/Component/Label/AndongLabel.swift
+++ Tteolione/Sources/Component/Label/AndongLabel.swift
@@ -5,4 +5,21 @@
 //  Created by 전준영 on 1/10/25.
 //
 
-import Foundation
+import UIKit
+
+class AndongLabel: UILabel {
+    
+    init(text: String, color: UIColor) {
+        super.init(frame: .zero)
+        
+        self.text = text
+        font = Font.Andong18
+        textColor = color
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
