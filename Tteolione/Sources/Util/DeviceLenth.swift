c008870f30e534d12f6469869f344e425e24dfd4
JunYeong-J <eeot2827@gmail.com>
2025-01-10 17:53:50 +0900

    feat: 공통으로 사용할 디바이스 길이 추가

--- Tteolione/Sources/Util/DeviceLenth.swift
+++ Tteolione/Sources/Util/DeviceLenth.swift
@@ -5,4 +5,13 @@
 //  Created by 전준영 on 1/10/25.
 //
 
-import Foundation
+import UIKit
+
+struct Device {
+    static var screenWidth: CGFloat {
+        return UIScreen.main.bounds.width
+    }
+    static var screenHeight: CGFloat {
+        return UIScreen.main.bounds.height
+    }
+}
