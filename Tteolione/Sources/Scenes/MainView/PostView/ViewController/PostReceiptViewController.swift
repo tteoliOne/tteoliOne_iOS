b850b42ef546a13f0ea6eba0b9b094c88a80b687
JunYeong-J <eeot2827@gmail.com>
2025-01-10 23:24:34 +0900

    feat: 글쓰기중 영수증 화면 UI 구성

--- Tteolione/Sources/Scenes/MainView/PostView/ViewController/PostReceiptViewController.swift
+++ Tteolione/Sources/Scenes/MainView/PostView/ViewController/PostReceiptViewController.swift
@@ -5,4 +5,23 @@
 //  Created by 전준영 on 1/10/25.
 //
 
-import Foundation
+import UIKit
+import ReactorKit
+import RxCocoa
+
+final class PostReceiptViewController: BaseViewController<PostReceiptView> {
+    
+    var disposeBag = DisposeBag()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.modalPresentationStyle = .pageSheet
+        
+        if let sheet = self.sheetPresentationController {
+            sheet.detents = [.medium()]
+            sheet.preferredCornerRadius = 20
+            sheet.largestUndimmedDetentIdentifier = .large
+        }
+    }
+    
+}
