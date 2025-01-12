4ebc730ea2d5b4748bf1e7bc578a3c93ea072170
JunYeong-J <eeot2827@gmail.com>
2025-01-13 00:38:14 +0900

    feat: 탭바 화면 구성

--- Tteolione/Sources/Scenes/TabBar/TabBarController.swift
+++ Tteolione/Sources/Scenes/TabBar/TabBarController.swift
@@ -5,4 +5,19 @@
 //  Created by 전준영 on 1/13/25.
 //
 
-import Foundation
+import UIKit
+
+final class TabBarController: UITabBarController {
+    
+    init() {
+        super.init(
+            nibName: nil,
+            bundle: nil
+        )
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
