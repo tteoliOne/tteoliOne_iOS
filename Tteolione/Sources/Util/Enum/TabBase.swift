290b7187e7fda0fc5ce65744eefbdaa1cb8c8032
JunYeong-J <eeot2827@gmail.com>
2025-01-13 00:38:26 +0900

    feat: tabBase 구현

--- Tteolione/Sources/Util/Enum/TabBase.swift
+++ Tteolione/Sources/Util/Enum/TabBase.swift
@@ -5,4 +5,32 @@
 //  Created by 전준영 on 1/13/25.
 //
 
-import Foundation
+import UIKit
+
+enum TabBase: Int, CaseIterable {
+    case main, chat, myProfile
+    
+    var tabTitle: String {
+        switch self {
+        case .main: return "홈"
+        case .chat: return "채팅"
+        case .myProfile: return "내 정보"
+        }
+    }
+    
+    var unselectedImage: UIImage? {
+        switch self {
+        case .main: return UIImage(systemName: "house")
+        case .chat: return UIImage(systemName: "message")
+        case .myProfile: return UIImage(systemName: "person")
+        }
+    }
+    
+    var selectedImage: UIImage? {
+        switch self {
+        case .main: return UIImage(systemName: "house.fill")
+        case .chat: return UIImage(systemName: "message.fill")
+        case .myProfile: return UIImage(systemName: "person.fill")
+        }
+    }
+}
