98798d3daf7ea3fb890856c0a655742f7e96ae48
JunYeong-J <eeot2827@gmail.com>
2025-01-13 03:06:32 +0900

    feat: 네비게이션 상단바 구현

--- Tteolione/Sources/Coordinator/TabBar/Main/MainCoordinator.swift
+++ Tteolione/Sources/Coordinator/TabBar/Main/MainCoordinator.swift
@@ -19,6 +19,47 @@ final class MainCoordinator: Coordinator {
     
     func start() {
         let mainViewController = MainViewController()
+        configureNavBarAppearance()
+        configureNavBarButtons(for: mainViewController)
         navigationController.setViewControllers([mainViewController], animated: false)
     }
+}
+
+extension MainCoordinator {
+    
+    private func configureNavBarAppearance() {
+        let navigationBarAppearance = UINavigationBarAppearance()
+        navigationBarAppearance.configureWithOpaqueBackground()
+        navigationBarAppearance.titleTextAttributes = [
+            .foregroundColor: UIColor.myAppMain,
+            .font: Font.Andong25
+        ]
+        
+        navigationController.navigationBar.standardAppearance = navigationBarAppearance
+        navigationController.navigationBar.scrollEdgeAppearance = navigationBarAppearance
+        navigationController.navigationBar.compactAppearance = navigationBarAppearance
+        navigationController.navigationBar.isTranslucent = true
+        navigationController.navigationBar.tintColor = .myAppBlack
+    }
+    
+    private func configureNavBarButtons(for viewController: UIViewController) {
+        let leftButton = UIButton(type: .system)
+        leftButton.setTitle("내 이", for: .normal)
+        leftButton.setImage(UIImage(systemName: "chevron.down"), for: .normal)
+        leftButton.frame = CGRect(x: 0, y: 0, width: 70, height: 30)
+        leftButton.setTitleColor(.black, for: .normal)
+        leftButton.showsMenuAsPrimaryAction = true
+        viewController.navigationItem.leftBarButtonItem = UIBarButtonItem(customView: leftButton)
+        
+        let rightButton = UIBarButtonItem(
+            image: UIImage(systemName: "magnifyingglass"),
+            style: .plain,
+            target: self,
+            action: nil
+        )
+        viewController.navigationItem.rightBarButtonItem = rightButton
+        viewController.title = NavigationTitle.main.title
+        viewController.navigationItem.largeTitleDisplayMode = .never
+    }
+    
 }
