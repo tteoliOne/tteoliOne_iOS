2a57934e6eb9992870751e867e846745362ab7d0
JunYeong-J <eeot2827@gmail.com>
2025-01-14 16:34:39 +0900

    style: 로그인 버튼 색상 변경

--- Tteolione/Sources/Scenes/LoginView/View/LoginView.swift
+++ Tteolione/Sources/Scenes/LoginView/View/LoginView.swift
@@ -84,7 +84,7 @@ final class LoginView: BaseView {
     // MARK: - 로그인버튼
     lazy var loginButton = CommonButton(title: .login,
                                         corner: 8,
-                                        backgroundColor: .myAppLightGray,
+                                        backgroundColor: .myAppDarkGray,
                                         textColor: .white)
     
     // 이메일, 패스워드, 로그인버튼 스택뷰에 배치
