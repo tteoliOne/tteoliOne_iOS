042129b7ca0271adfa8ebc3cabe54d234e194f01
JunYeong-J <eeot2827@gmail.com>
2025-01-16 16:07:03 +0900

    chore: 파일 삭제

--- Tteolione/Sources/Presentation/OnboardingScenes/FindAccountScene/Coordinator/Protocol/FindAccountCoordinatorDelegate.swift
+++ Tteolione/Sources/Presentation/OnboardingScenes/FindAccountScene/Coordinator/Protocol/FindAccountCoordinatorDelegate.swift
@@ -10,8 +10,8 @@ import Foundation
 protocol FindAccountCoordinatorDelegate: Coordinator {
     func pushFindIdViewController()
     func pushResultIdViewController(with resultData: FindIDDTO)
-    func pushResetPasswordChcekViewController()
+    func pushResetPasswordChcekViewController(viewType: AccountCoordinator)
     func pushAuthViewController(viewType: AccountCoordinator)
     func pushResetPasswordViewController()
-    func finish()
+    func finishView()
 }
