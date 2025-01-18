295e4425f19a2f6aa31709dfc8ed5cd52b5e43a7
JunYeong-J <eeot2827@gmail.com>
2025-01-18 21:33:02 +0900

    feat: numberFormatter 추가

--- Tteolione/Sources/Util/Manager/FormatterManager.swift
+++ Tteolione/Sources/Util/Manager/FormatterManager.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+final class FormatterManager {
+    
+    static let shared = FormatterManager()
+    
+    private init() { }
+    
+    func numberFormatter(_ data: Int) -> String {
+        let format = NumberFormatter()
+        format.numberStyle = .decimal
+        return format.string(from: NSNumber(value: data)) ?? "\(data)"
+    }
+    
+}
