f2daac8f070fe45ce55aae67333632972c2b4513
JunYeong-J <eeot2827@gmail.com>
2025-01-18 21:33:34 +0900

    feat: Reactive + Extension 추가

--- Tteolione/Sources/Util/Extension/Reactive+Extension.swift
+++ Tteolione/Sources/Util/Extension/Reactive+Extension.swift
@@ -1,8 +1,26 @@
 //
-//  UIViewController+Extension.swift
+//  Reactive+Extension.swift
 //  Tteolione
 //
 //  Created by 전준영 on 1/18/25.
 //
 
-import Foundation
+import UIKit
+import RxSwift
+
+extension Reactive where Base: UIViewController {
+    var viewWillAppear: Observable<Void> {
+        return methodInvoked(#selector(UIViewController.viewWillAppear(_:)))
+            .map { _ in }
+    }
+
+    var viewDidAppear: Observable<Void> {
+        return methodInvoked(#selector(UIViewController.viewDidAppear(_:)))
+            .map { _ in }
+    }
+    
+    var viewWillDisappear: Observable<Void> {
+        return methodInvoked(#selector(UIViewController.viewWillDisappear(_:)))
+            .map { _ in }
+    }
+}
