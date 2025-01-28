4d538a8374d5d99b3d62ac2aca97a629252e2b22
JunYeong-J <eeot2827@gmail.com>
2025-01-28 16:46:55 +0900

    feat: component 추가

--- Tteolione/Sources/Component/Button/XButton.swift
+++ Tteolione/Sources/Component/Button/XButton.swift
@@ -5,4 +5,22 @@
 //  Created by 전준영 on 1/28/25.
 //
 
-import Foundation
+import UIKit
+
+class XButton: UIButton {
+    
+    init(color: UIColor) {
+        super.init(frame: .zero)
+        
+        let imageConfig = UIImage.SymbolConfiguration(pointSize: 24, weight: .light)
+        setImage(UIImage(systemName: "multiply", withConfiguration: imageConfig)?
+            .withTintColor(color, renderingMode: .alwaysOriginal), for: .normal)
+        imageView?.contentMode = .scaleAspectFit
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
--- Tteolione/Sources/Component/UISearchBar/SearchBar.swift
+++ Tteolione/Sources/Component/UISearchBar/SearchBar.swift
@@ -5,4 +5,29 @@
 //  Created by 전준영 on 1/28/25.
 //
 
-import Foundation
+import UIKit
+
+class SearchBar: UISearchBar {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupSearchBar()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    private func setupSearchBar() {
+        self.placeholder = "검색하기"
+        
+        if let searchTextField = self.value(forKey: "searchField") as? UITextField {
+            self.tintColor = UIColor(red: 0x58/255.0, green: 0x8F/255.0, blue: 0x11/255.0, alpha: 1.0)
+            searchTextField.backgroundColor = .white
+            searchTextField.layer.cornerRadius = 15
+            searchTextField.layer.borderWidth = 1.0
+            searchTextField.layer.borderColor = UIColor(red: 0.8255520463, green: 0.835944593, blue: 0.8357618451, alpha: 1).cgColor
+        }
+    }
+    
+}
