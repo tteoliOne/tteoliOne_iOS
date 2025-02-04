9d073deb254ea7ed7cfc2efa37e0e24bacffe480
JunYeong-J <eeot2827@gmail.com>
2025-02-04 15:25:18 +0900

    refactor: x버튼 수정

---
+++ Tteolione/Sources/Presentation/MainScenes/SideMenuScene/Reactor/SideMenuReactor.swift
@@ -0,0 +1,8 @@
+//
+//  SideMenuReactor.swift
+//  Tteolione
+//
+//  Created by 전준영 on 2/4/25.
+//
+
+import Foundation
---
+++ Tteolione/Sources/Presentation/MainScenes/SideMenuScene/View/SideMenuTableViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  SideMenuTableViewCell.swift
+//  Tteolione
+//
+//  Created by 전준영 on 2/4/25.
+//
+
+import Foundation
